f4bf39e87ecf75095b8319b551a6cf49ef56f40e
David Kennedy S. Araujo <david.araujo@zoop.co>
2018-06-19 14:09:49 -0300

    Split time

--- src/mylib.cc
+++ src/mylib.cc
@@ -2,9 +2,17 @@
 
 namespace my::time {
 
+std::tuple<std::chrono::seconds, size_t> 
+  SplitTime(const size_t seconds) {
+    return { std::chrono::seconds(seconds/10), 10 };
+  }
+
 void MySleep() {
-  using namespace std::chrono_literals;
-  std::this_thread::sleep_for(10s);
+  auto [ interval, multiplier ] = SplitTime(10);
+
+  for (size_t i = 0; i < multiplier; ++i)
+    std::this_thread::sleep_for(interval);
+
 }
 
 };
